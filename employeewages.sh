d650414dc49c4d391ec1b93b2dc1d165377cd111
prakhar00100 <prakhar00100@gmail.com>
2020-10-27 23:40:35 -0700

    add

--- employeewages.sh
+++ employeewages.sh
@@ -1,6 +1,14 @@
+
 #!/bin/bash -x
 
-#variables
+
+employee=$((RANDOM%2))
+
+ if [ $employee -eq 1 ];
+ then
+
+ echo "employee is present" $employee
+
 Wage_per_hour=20
 
 day_hour=8
@@ -9,3 +17,8 @@ Daily_employee-wage=$(($Wage_per_hour*$day_hour))
 
 echo "Daily  employee wage - "$Daily_employee_wage
 
+
+ else
+
+ echo "employee is absent" $employee
+ fi
